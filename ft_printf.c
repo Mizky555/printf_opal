fa0acb37e537de7e642a55f9092cb25cf987e1ae
Thanaporn Sirirakwongsa <tsirirak@z1t5c2.42bangkok.com>
2022-05-08 23:44:14 +0700

    P'pol2

--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,29 @@
 /*   By: tsirirak <tsirirak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 15:01:12 by tsirirak          #+#    #+#             */
-/*   Updated: 2022/05/08 22:27:11 by tsirirak         ###   ########.fr       */
+/*   Updated: 2022/05/08 23:42:38 by tsirirak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int ft_printf(const char *fmt, ...)
 {
-    va_list ap;
+    va_list va_list_ap;
     int     len;
+    int     fmt_len;
+    char    *string_recieved;
+    char    *malloc_string;
 
     len = 0;
+    fmt_len = 0;
     if (fmt == NULL)
         return (0);
-    va_start(ap, fmt);
+    va_start(va_list_ap, fmt);
     while (*fmt)
     {
         if (*fmt != '%')
@@ -33,15 +39,43 @@ int ft_printf(const char *fmt, ...)
             // 2 = stderr (error)
             // etc. = opened file
             write(1, fmt, 1);
+            len++;
+        }
+        else //*fmt == '%'
+        {
+            if (*(fmt+1) == '%')
+            {
+                write(1, "%", 1);
+                len++;
+            }
+            if (*(fmt+1) == 's')
+            {
+                string_recieved = va_arg(va_list_ap, char *);
+                fmt_len = strlen(string_recieved);
+                // malloc_string = (char *)malloc(sizeof(char) * (fmt_len + 1));
+                // malloc_string = strncpy(malloc_string, string_recieved, fmt_len);
+                // malloc_string[fmt_len] = '\0';
+                write(1, string_recieved, fmt_len);
+                len += fmt_len;
+                fmt++;
+            }
         }
         fmt++;
-        len++;
     }
-    va_end(ap);
+    va_end(va_list_ap);
     return (len);
 }
 
 int main(void)
 {
-    ft_printf("Hello World\n");
+    ft_printf("Hello %% World %% |%s|\n", "This is from %s");
+    // printf("+42 = %+5d", 42);
 }
+
+// %[flags => "-+ #0"][width][.precision]specifier
+/*
+{
+    - is_left
+    + is_signed
+}
+*/
--- poniter.c
+++ poniter.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <strings.h>
 
 int main(void)
 {
     int a = 42;
     int *b = &a;
 
-    printf("a = %d\n", a);
-    printf("&a = %p\n", &a);
-    printf("b = %p\n", b);
-    printf("&b = %p\n", &b);
-    printf("*b = %d\n", *b);
+    // printf("a = %d\n", ++a);
+    // printf("a = %d\n", a);
+    // printf("&a = %p\n", &a);
+    // printf("b = %p\n", b);
+    // printf("&b = %p\n", &b);
+    // printf("*b = %d\n", *b);
+
+    char str[100];
+    char *ptr;
+    char target[100] = "0123456789";
+    char *p_t;
+
+    ptr = NULL;
+    ptr = target;
+    
+    while (*ptr)
+    {
+        // *ptr++
+        // 1 -- *ptr read value inside address that ptr point to
+        // 2 -- ++ =>>> ptr = ptr +1, increment value inside ptr 1,
+        printf("before addr value in ptr = %p\n", ptr++);
+        // printf("print value in *ptr++ = %c\n", ptr++);
+        printf("after addr value in ptr  = %p\n", ptr);
+        printf("address of ptr = %p\n\n", &ptr);
+    }
 }
